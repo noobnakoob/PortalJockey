958fe4286507d1209f109cbac84595a972f34b36
noobnakoob <aca.rampage@gmail.com>
2017-09-23 22:19:33 +0200

    gfg

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -7,22 +7,28 @@ public class EnemyMovement : MonoBehaviour {
     public float speed;
 
     private Vector3 endPosition;
-    private float angle;
+    private float angleRot;
+    private float angleMove;
     private float sinAngle;
     private float cosAngle;
 
 	// Use this for initialization
 	void Start () {
 
-        angle = Random.Range(0, 360);
-        sinAngle = Mathf.Sin(angle);
-        cosAngle = Mathf.Cos(angle);
+        angleRot = Random.Range(0, 360);
+        angleMove = Mathf.Deg2Rad * angleRot;
+        sinAngle = Mathf.Sin(angleMove);
+        Debug.Log("SIN: " + sinAngle);
+        cosAngle = Mathf.Cos(angleMove);
+        Debug.Log("COS: " + cosAngle);
+        //transform.rotation = Quaternion.AngleAxis(360 - angle, Vector3.up);
+        transform.Rotate(new Vector3(0, angleRot - 180, 0));
         //endPosition = new Vector3(Mathf.Cos(angle) * 100, Mathf.Sin(angle) * 100, transform.position.z);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 
-        transform.position = new Vector3(transform.position.x + cosAngle * speed, transform.position.y , transform.position.z + sinAngle * speed);
+        transform.position = new Vector3(transform.position.x + sinAngle * speed, transform.position.y , transform.position.z + cosAngle * speed);
     }
 }
